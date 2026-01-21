2b9bd31463a432c73f8c2ae87105336bdf593a10
Alex <alexprokopov@gmail.com>
2026-01-21 14:30:04 +0200

    default timeout change

--- custom_components/power_watchdog/const.py
+++ custom_components/power_watchdog/const.py
@@ -3,10 +3,8 @@ DOMAIN = "power_watchdog"
 CONF_TELEGRAM_TOKEN = "telegram_token"
 CONF_TELEGRAM_CHAT_ID = "telegram_chat_id"
 
-# Старый ключ (оставляем для обратной совместимости)
 CONF_ENTITY_ID = "entity_id"
 
-# Новый ключ: теперь контролируем именно датчик напряжения (sensor.*voltage)
 CONF_VOLTAGE_ENTITY_ID = "voltage_entity_id"
 
 CONF_DEBOUNCE_SECONDS = "debounce_seconds"
@@ -15,4 +13,4 @@ DEFAULT_DEBOUNCE_SECONDS = 10
 OFFLINE_STATES = {"unavailable", "unknown", "offline"}
 
 CONF_STALE_TIMEOUT_SECONDS = "stale_timeout_seconds"
-DEFAULT_STALE_TIMEOUT_SECONDS = 180  # 3 минуты
+DEFAULT_STALE_TIMEOUT_SECONDS = 300
